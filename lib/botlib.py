5e2ded13cd8a34451a5cc5e5c67305a24e1b78b4
Nipuna Perera <nipunap@gmail.com>
2021-09-23 00:50:21 +0100

    errors fixed in git_pull

--- lib/botlib.py
+++ lib/botlib.py
@@ -16,5 +16,5 @@ def restart_service(service):
 def git_pull():
     bot_home = '/root/orchestrator'
     g = git.cmd.Git(bot_home)
-    g.heads.main.checkout()
+    g.checkout('main')
     g.pull()
